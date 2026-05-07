47cc087d9502487f8e9046965e1910ef4305dfc7
sergiresina-gif <sergi.resina.xirgu@gmail.com>
2026-05-07 14:53:55 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+
+# Input of N (number of inhabitants) and k (neighbours)
+print( "Input number of nodes (N): ")
+N = input()
+print( "Input number of neighbours (k): ")
+k = input()
+
+print("Input what network generating algorythm you want:")
+print( "1. asdasd \n 2. asdsadas")
+choice = input()
+
+match choice:
+    case 1:
+        pass
+    case 2:
+        pass
+
+
