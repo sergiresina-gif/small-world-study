dc459605a612d264d79693475867a483a8e24184
sergiresina-gif <sergi.resina.xirgu@gmail.com>
2026-05-07 15:51:12 +0200

    changes to architecture

--- main.py
+++ main.py
@@ -10,25 +10,31 @@ import matplotlib.pyplot as plt
 N = 10
 k = 4
 
-print("Input what network generating algorythm you want:")
-print( "1. Xarxa en anell regular \n2. Erdös-Rényi \n3. Watts-Strogatz")
-choice = int(input())
+# print("Input what network generating algorythm you want:")
+# print( "1. Xarxa en anell regular \n2. Erdös-Rényi \n3. Watts-Strogatz")
+# choice = int(input())
 
 # A = nx.Graph()
 # for node in range(N):
 #     A.add_node(f"{node}")
 
-match choice:
-    case 1:
-        A = nx.Graph()
-        for node in range(N):
-            for neighbour_right in range(1,k//2+1):
-                A.add_edge(node,( node+neighbour_right )%N )
-                A.add_edge(node,( node - neighbour_right )%N )
-    case 2:
-        pass
+# Cas a
+def anell_regular(N,k):
+    A = nx.Graph()
+    for node in range(N):
+        for neighbour_right in range(1,k//2+1):
+            A.add_edge(node,( node+neighbour_right )%N )
+            A.add_edge(node,( node - neighbour_right )%N )
+    return A
 
+# match choice:
+#     case 1:
+#         A = anell_regular(N,k)
+#     case 2:
+#         pass    
+
+A = anell_regular(N,k)
 nx.draw_spring(A, with_labels=True)
 plt.show()
 
-print()
+
