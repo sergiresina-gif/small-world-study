118d3d307134f60739cfaadca9f6298f6e8f7338
sergiresina-gif <sergi.resina.xirgu@gmail.com>
2026-05-07 15:39:34 +0200

    added Xarxa en anell regular

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
+import networkx as nx
+import matplotlib.pyplot as plt
 
 # Input of N (number of inhabitants) and k (neighbours)
-print( "Input number of nodes (N): ")
-N = input()
-print( "Input number of neighbours (k): ")
-k = input()
+# print( "Input number of nodes (N): ")
+# N = int(input())
+# print( "Input number of neighbours (k): ")
+# k = int(input())
+
+N = 10
+k = 4
 
 print("Input what network generating algorythm you want:")
-print( "1. asdasd \n 2. asdsadas")
-choice = input()
+print( "1. Xarxa en anell regular \n2. Erdös-Rényi \n3. Watts-Strogatz")
+choice = int(input())
+
+# A = nx.Graph()
+# for node in range(N):
+#     A.add_node(f"{node}")
 
 match choice:
     case 1:
-        pass
+        A = nx.Graph()
+        for node in range(N):
+            for neighbour_right in range(1,k//2+1):
+                A.add_edge(node,( node+neighbour_right )%N )
+                A.add_edge(node,( node - neighbour_right )%N )
     case 2:
         pass
 
+nx.draw_spring(A, with_labels=True)
+plt.show()
 
+print()
